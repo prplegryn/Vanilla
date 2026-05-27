71042b0185f8340aa7597f8a7ed350a296be62b7
prplegryn <galacticequus@gmail.com>
2026-05-27 23:25:41 +0800

    Fix crash screen LocalContext import

--- app/src/main/java/com/example/vanilla/MainActivity.kt
+++ app/src/main/java/com/example/vanilla/MainActivity.kt
@@ -74,6 +74,7 @@ import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
